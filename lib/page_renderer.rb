8028ca7c8a95abaa761a6cb7683357cd46127ccf
Dan Q <dan@danq.me>
2017-11-15 16:09:50 +0000

    Static page headers/footers.

--- lib/page_renderer.rb
+++ lib/page_renderer.rb
@@ -32,11 +32,21 @@ module PageRenderer
 
   protected
 
-  def self.render(template_name, library, library_binding, path)
+  def self.render(template_name, library, library_binding, path, output_filename = nil)
+    # Get header and footer for this content type, if available
+    super_template_name = template_name.split('.')[1..-1].join('.') # suffix, i.e. index.html.erb becomes html.erb
+    header_template_file_name = File.join('templates', "header.#{super_template_name}.erb")
+    footer_template_file_name = File.join('templates', "footer.#{super_template_name}.erb")
+    header = File::exists?(header_template_file_name) ? ERB.new(File.read(header_template_file_name)).result(library_binding) : ''
+    footer = File::exists?(footer_template_file_name) ? ERB.new(File.read(footer_template_file_name)).result(library_binding) : ''
+    # Get content and render to file specified by output_filename, or same as template_name if not specified
     template_file = File.join('templates', "#{template_name}.erb")
     template = ERB.new(File.read(template_file))
-    File.open(File.join(path, template_name), 'w') do |f|
-      f.puts template.result(library_binding)
+    output_filename ||= template_name
+    File.open(File.join(path, output_filename), 'w') do |f|
+      f.print header
+      f.print template.result(library_binding)
+      f.print footer
     end
   end
 end
